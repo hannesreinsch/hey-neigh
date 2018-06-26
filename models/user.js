4458bd49c6fd21064aaf23a604aae260529c71e1
Laure Dassy <laure.dassy@hotmail.com>
2018-06-26 19:08:35 +0200

    google map sign up

--- models/user.js
+++ models/user.js
@@ -4,19 +4,20 @@ const Schema = mongoose.Schema;
 
 //the user schema
 const userSchema = new Schema({
-  firstNeighm: {type: String,},
-  lastNeighm: {type: String,},
-  address: {type: String, required: true},
-  email: {type: String, required: true},
-  password: {type: String, required: true},
-})
+  firstNeighm: { type: String },
+  lastNeighm: { type: String },
+  address: { type: String, required: true },
+  location: { type: { type: String }, coordinates: [Number] },
+  email: { type: String, required: true },
+  password: { type: String, required: true }
+});
+userSchema.index({ location: "2dsphere" });
 
 //set timestamps for the user schema
 userSchema.set("timestamps", true);
 
 //create user model with user schema
 const User = mongoose.model("User", userSchema);
-
 
 //export the model so we can use it in other files
 module.exports = User;
--- routes/edit.js
+++ routes/edit.js
@@ -13,133 +13,118 @@ const bcrypt = require("bcrypt");
 
 const bcryptSalt = 10;
 
-
-
 //############################################################
 //##########################  EDIT  ##########################
 //############################################################
-//render the edit view when accessing the /edit route 
+//render the edit view when accessing the /edit route
 router.get("/edit", (req, res, next) => {
-    //if user is not logged in, redirect to homepage
-    if (!req.session.currentUser) {
-      res.redirect('/');
-      return;
-    }
-  
-    res.render("edit", {
-      errorMessage: ""
-  });
-})
-
-
-//runs when user submits the form
-router.post("/edit", (req, res, next) => {
-
- //save the ID to update it's user  
- const userId = req.session.currentUser._id;
- //save inputs from form to update
- const password = req.body.password;
- const email = req.body.email;
- const firstNeighm = req.body.firstNeighm;
- const lastNeighm = req.body.lastNeighm;
- const address = req.body.address;
- //encrypt password
- const salt = bcrypt.genSaltSync(bcryptSalt);
- const hashedPass = bcrypt.hashSync(password, salt);
- 
-
- //object to update
- const userInfo = {
-    firstNeighm: firstNeighm,
-    lastNeighm:  lastNeighm,
-    email:       email,
-    address:     address,
-    password:    hashedPass,
-  };
-
-  
-  //when fields are empty, display error
-  if(email === "" || address === "" || password === "" || firstNeighm === "" || lastNeighm === ""){
-    res.render("edit", {
-      errorMessage: "Please enter a field to change",
-    })
-    return;
-  }
-  
-    //if user doesnt exist, display error
-    if(userId === null){
-      res.render("edit", {
-        errorMessage: `There is no User with the E-Mail: "${email}". Please Sign Up.`
-      })
-      return;
-    }
-    
-    
-  
-      //find user by id and update
-      User.findByIdAndUpdate(userId, userInfo, { new: true }, (err, theUser) => {
-        if (err) {
-          next(err);
-          return;
-        }
-    
-        req.session.currentUser = theUser;
-    
-        res.redirect('/');
-
-   });
- })
-
-
-
-
-
-//############################################################
-//##########################  DELETE  ##########################
-//############################################################
-//render the edit view when accessing the /delete route 
-router.get("/delete", (req, res, next) => {
   //if user is not logged in, redirect to homepage
   if (!req.session.currentUser) {
-    res.redirect('/');
+    res.redirect("/");
     return;
   }
 
   res.render("edit", {
     errorMessage: ""
+  });
 });
-})
 
+//runs when user submits the form
+router.post("/edit", (req, res, next) => {
+  //save the ID to update it's user
+  const userId = req.session.currentUser._id;
+  //save inputs from form to update
+  const password = req.body.password;
+  const email = req.body.email;
+  const firstNeighm = req.body.firstNeighm;
+  const lastNeighm = req.body.lastNeighm;
+  const address = req.body.address;
+  //encrypt password
+  const salt = bcrypt.genSaltSync(bcryptSalt);
+  const hashedPass = bcrypt.hashSync(password, salt);
+
+  //object to update
+  const userInfo = {
+    firstNeighm: firstNeighm,
+    lastNeighm: lastNeighm,
+    email: email,
+    address: address,
+    password: hashedPass
+  };
+
+  //when fields are empty, display error
+  if (
+    email === "" ||
+    address === "" ||
+    password === "" ||
+    firstNeighm === "" ||
+    lastNeighm === ""
+  ) {
+    res.render("edit", {
+      errorMessage: "Please enter a field to change"
+    });
+    return;
+  }
+
+  //if user doesnt exist, display error
+  if (userId === null) {
+    res.render("edit", {
+      errorMessage: `There is no User with the E-Mail: "${email}". Please Sign Up.`
+    });
+    return;
+  }
+
+  //find user by id and update
+  User.findByIdAndUpdate(userId, userInfo, { new: true }, (err, theUser) => {
+    if (err) {
+      next(err);
+      return;
+    }
+
+    req.session.currentUser = theUser;
+
+    res.redirect("/");
+  });
+});
+
+//############################################################
+//##########################  DELETE  ##########################
+//############################################################
+//render the edit view when accessing the /delete route
+router.get("/delete", (req, res, next) => {
+  //if user is not logged in, redirect to homepage
+  if (!req.session.currentUser) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("edit", {
+    errorMessage: ""
+  });
+});
 
 //runs when user clicks the delete button
 router.post("/delete", (req, res, next) => {
-
-  //save the ID to delete it's user  
+  //save the ID to delete it's user
   const userId = req.session.currentUser._id;
 
-
   //if user doesnt exist, display error
-  if(userId === null){
+  if (userId === null) {
     res.render("edit", {
       errorMessage: `There is no User with the E-Mail: "${email}". Please Sign Up.`
-    })
+    });
     return;
   }
-  
 
-    //find user by id and delete
-    User.findByIdAndRemove(userId, (err, theUser) => {
-      if (err) {
-        next(err);
-        return;
-      }
-  
-      res.redirect('/logout');
+  //find user by id and delete
+  User.findByIdAndRemove(userId, (err, theUser) => {
+    if (err) {
+      next(err);
+      return;
+    }
 
- });
-})
-
-
-
+    res.redirect("/logout");
+  });
+});
 
 module.exports = router;
