c662e544a3476a52f4f38393a37b81a3043765b2
Laure Dassy <laure.dassy@hotmail.com>
2018-06-27 11:21:52 +0200

    go back

--- routes/add-post.js
+++ routes/add-post.js
@@ -11,71 +11,57 @@ const Post = require("../models/post");
 
 const router = express.Router();
 
-
-
-
 //############################################################
 //##########################  POST  ##########################
 //############################################################
-//render the signup view when accessing the /signup route 
+//render the signup view when accessing the /signup route
 router.get("/add-post", (req, res, next) => {
-    //if user is not logged in, redirect to homepage
-    if (!req.session.currentUser) {
-      res.redirect('/');
-      return;
-    }
-
+  //if user is not logged in, redirect to homepage
+  if (!req.session.currentUser) {
+    res.redirect("/");
+    return;
+  }
   res.render("index", {
     errorMessage: ""
   });
-})
-
+});
 
 //adds post to database
 router.post("/add-post", (req, res, next) => {
+  //save the inputs from the form in variables
+  let text = req.body.text;
+  let owner = req.session.currentUser._id;
+  let location = req.session.currentUser.location;
+  let comments = [];
 
- //save the inputs from the form in variables
- let text = req.body.text;
- let owner = req.session.currentUser._id;
- let comments = [];
- 
- //when field is empty, display error
- if(text === ""){
-   res.render("index", {
-     errorMessage: "Please write something to post",
-   })
-   return;
- }
+  //when field is empty, display error
+  if (text === "") {
+    res.render("index", {
+      errorMessage: "Please write something to post"
+    });
+    return;
+  }
 
-   //save input values in post object to save later 
-   const postSubmission = {
-     text: text,
-     _owner: owner,
-     _comments: comments,
-   }
+  //save input values in post object to save later
+  const postSubmission = {
+    text: text,
+    _owner: owner,
+    _comments: comments
+  };
 
-   //create new instance
-   const thePost = new Post(postSubmission);
-   //save the instance in database
-   thePost.save((err) => {
-     if (err) {
-       res.render('index', {
-        errorMessage: 'Something went wrong. Try again later.'
-       });
-       return;
-     }
+  //create new instance
+  const thePost = new Post(postSubmission);
+  //save the instance in database
+  thePost.save(err => {
+    if (err) {
+      res.render("index", {
+        errorMessage: "Something went wrong. Try again later."
+      });
+      return;
+    }
 
-     res.redirect('/');
-   });
- })
-
-
-
-
-
-
-
-
-
+    res.redirect("/");
+  });
+});
 
 module.exports = router;
