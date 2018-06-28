93bbb2d583204392b811e7c3f18cd471a6322645
Hannes Reinsch <hannes.reinsch99@gmail.com>
2018-06-28 15:20:31 +0200

    click on name from post or comment owner and get redirected to his profile

--- routes/profile.js
+++ routes/profile.js
@@ -8,19 +8,23 @@ const User = require("../models/user");
 const router = express.Router();
 
 //render the profile view when accessing the /profile route
-router.get("/profile", (req, res, next) => {
+router.get("/profile/:id", (req, res, next) => {
   //if theres no user logged in, redirect to homepage
   if (!req.session.currentUser) {
     res.redirect("/");
     return;
   }
 
-  let userId = req.params._id;
-  console.log(userId);
+  let userId = req.params.id;
   
-  // User.findById()
-
-  res.render("profile");
-});
+  console.log(userId);
+  User.findOne({'_id': userId})
+    .then(user => {
+      res.render("profile", { user })
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  })
 
 module.exports = router;
